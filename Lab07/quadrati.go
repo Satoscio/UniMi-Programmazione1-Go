16ec420508aa83ed5f32884f83de6158d206e2e7
Satoscio <satoscio65@gmail.com>
2022-12-12 08:43:01 +0100

    non so leggere

--- Lab07/quadrati.go
+++ Lab07/quadrati.go
@@ -3,28 +3,29 @@ package main
 import (
 	"fmt"
 	"math"
+	"os"
+	"strconv"
 )
 
 
 func isSquare(n int) bool {
     x := math.Sqrt(float64(n))
     z := math.Mod(x, float64(1))
-    return !(z != 0)
+    return z == 0
 }
 
 func main() {
-    var n int
-    var ns = []int{}
+    var nss = []string{}
+    var nsn = []int{}
 
-    for {
-        num, _ := fmt.Scan(&n)
-        if num == 0 {
-            break
-        }
-        if isSquare(n) {
-            ns = append(ns, n)
+    nss = os.Args[1:]
+    
+    for _, n := range nss {
+        i, _ := strconv.Atoi(n)
+        if isSquare(i) {
+            nsn = append(nsn, i)
         }
     }
 
-    fmt.Println(ns)
+    fmt.Println(nsn)
 }
